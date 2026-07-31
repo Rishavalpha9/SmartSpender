78e0111079c00ff515b1bdd273cab1679c7c0441
Rishav kumar <132774211+Rishavalpha9@users.noreply.github.com>
2026-07-31 09:34:58 +0530

    Update footer credit from Rajeev Ranajn to Rishav Raj

--- app/layout.js
+++ app/layout.js
@@ -29,7 +29,7 @@ export default function RootLayout({ children }) {
                 <footer className="bg-blue-50 py-12">
                     <div className="container mx-auto px-4 text-center text-gray-600">
                    <p>
-                    Made with 💗 Rajeev Ranajn
+                    Made with 💗 Rishav Raj
                    </p>
                     </div>
                 </footer>
